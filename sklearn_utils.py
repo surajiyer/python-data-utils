ad5b241b1e58be3882d5fe6f39a970daa15f6c42
Suraj Iyer <suraj.iyer96@outlook.com>
2019-01-14 15:55:39 +0100

    Update sklearn_utils.py
    - Added sklearn linear reg. model with t-stats and p-values.

--- sklearn_utils.py
+++ sklearn_utils.py
@@ -10,6 +10,8 @@ from __future__ import print_function
 import six
 import numpy as np
 import pandas as pd
+from scipy import stats
+from sklearn import linear_model
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_validate as _cross_validate
@@ -29,6 +31,40 @@ def get_estimator_name(clf):
     :return: string name
     """
     return str(type(clf)).split('.')[-1].replace("'>", "")
+
+###############################################################################
+# SKLEARN CUSTOM ESTIMATORS
+###############################################################################
+
+class LinearRegression(linear_model.LinearRegression):
+    """
+    LinearRegression class after sklearn's, but calculate t-statistics
+    and p-values for model coefficients (betas).
+    Additional attributes available after .fit()
+    are `t` and `p` which are of the shape (y.shape[1], X.shape[1])
+    which is (n_features, n_coefs)
+    This class sets the intercept to 0 by default, since usually we include it
+    in X.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if not "fit_intercept" in kwargs:
+            kwargs['fit_intercept'] = False
+        super(LinearRegression, self)\
+                .__init__(*args, **kwargs)
+
+    def fit(self, X, y, n_jobs=1):
+        self = super(LinearRegression, self).fit(X, y, n_jobs)
+
+        sse = np.sum((self.predict(X) - y) ** 2, axis=0) / float(X.shape[0] - X.shape[1])
+        se = np.array([
+            np.sqrt(np.diagonal(sse[i] * np.linalg.inv(np.dot(X.T, X)))) 
+            for i in range(sse.shape[0])
+        ])
+
+        self.t = self.coef_ / se
+        self.p = 2 * (1 - stats.t.cdf(np.abs(self.t), y.shape[0] - X.shape[1]))
+        return self
 
 ###############################################################################
 # SKLEARN EVALUATION FUNCTIONS
