e86f346890ac3cbba6746fc5d30b252b9632e839
Suraj Iyer <suraj.iyer96@outlook.com>
2019-04-14 16:34:04 +0200

    Updated statsmodels logit summary function(s)

--- statsmodel_utils.py
+++ statsmodel_utils.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import statsmodels as statsm
 import statsmodels.formula.api as smf
 from statsmodels.graphics.gofplots import ProbPlot
+import re
 
 plt.style.use('seaborn') # pretty matplotlib plots
 # plt.rc('font', size=14)
@@ -253,44 +254,54 @@ def sum_of_squares_r2(results, labels, pos=1, neg=0):
     return top / bottom
 
 def logit_evaluation_summary(results, labels, pos=1, neg=0):
-	return pd.DataFrame([
-		('Pseudo R-squared', results.prsquared)
-		, ('Tjur R-squared', tjur_r2(results, labels, pos, neg))
-		, ('Squared Pearson Correlation R-squared', squared_pearson_correlation_r2(results, labels, pos, neg))
-		, ('Sum-of-squares R-squared', sum_of_squares_r2(results, labels, pos, neg))])
+    return pd.DataFrame([
+        ('Pseudo R-squared', results.prsquared)
+        , ('Tjur R-squared', tjur_r2(results, labels, pos, neg))
+        , ('Squared Pearson Correlation R-squared', squared_pearson_correlation_r2(results, labels, pos, neg))
+        , ('Sum-of-squares R-squared', sum_of_squares_r2(results, labels, pos, neg))])
 
-def logit_params_summary(results, alpha=0.05, alphas=np.array([0.1, 0.05, 0.01, 0.001]), margeff_kwargs={}):
-	assert all(isinstance(a, float) for a in alphas), 'ValueError: alphas.'
+def logit_summary_params(results, alpha=0.05, alphas=np.array([0.1, 0.05, 0.01, 0.001]), margeff_kwargs={}):
+    assert all(isinstance(a, float) and 0. <= a <= 1. for a in alphas), 'ValueError: alphas.'
+    assert all(a > b for a, b in zip(alphas, alphas[1:])), 'alphas must be in strictly increasing order.'
 
-	# get confidenc intervals for coefficients
-	conf_int = results.conf_int(alpha)
+    # # get confidenc intervals for coefficients
+    # conf_int = results.conf_int(alpha)
 
-	# get index of columns where nunique == 1
-	constant_cols_idx = np.argwhere(np.apply_along_axis(lambda x: np.unique(x).shape[0] == 1, 0, results.model.exog) == True).ravel()
+    # # get index of columns where nunique == 1
+    # constant_cols_idx = np.argwhere(np.apply_along_axis(lambda x: np.unique(x).shape[0] == 1, 0, results.model.exog) == True).ravel()
 
-	# get and adjust marginal effects table by adding nans for columns with constant values
-	margeff = np.insert(results.get_margeff(**margeff_kwargs).margeff, constant_cols_idx, np.nan)
+    # # get and adjust marginal effects table by adding nans for columns with constant values
+    # margeff = np.insert(results.get_margeff(**margeff_kwargs).margeff, constant_cols_idx, np.nan)
 
-	return pd.DataFrame(list(zip(results.params
-	                      , np.exp(results.params)
-	                      , results.bse
-	                      , margeff
-	                      , results.tvalues
-	                      , results.pvalues
-	                      , *conf_int.values.T
-	                      , results.pvalues.apply(lambda x: '*'*np.sum(x < alphas))))
-	             , columns=['Coef.', 'OddsRat.', 'Std.Err.', 'ME', 'z', 'P>|z|', '[{}'.format(alpha/2), '{}]'.format(1-(alpha/2)), 'Sign.']
-	             , index=conf_int.index)
+    # return pd.DataFrame(list(zip(results.params
+    #                       , np.exp(results.params)
+    #                       , results.bse
+    #                       , margeff
+    #                       , results.tvalues
+    #                       , results.pvalues
+    #                       , *conf_int.values.T
+    #                       , results.pvalues.apply(lambda x: '*'*np.sum(x < alphas))))
+    #              , columns=['Coef.', 'OddsRat.', 'Std.Err.', 'ME', 'z', 'P>|z|', '[{}'.format(alpha/2), '{}]'.format(1-(alpha/2)), 'Sign.']
+    #              , index=conf_int.index)
+    params = statsm.iolib.summary2.summary_params(results)
+    params.insert(1, 'OddsRat.', np.exp(params['Coef.']))
+    params.insert(2, 'ME', results.get_margeff(**margeff_kwargs).summary_frame()['dy/dx'])
+    params['Sign.'] = results.pvalues.apply(lambda x: '*'*np.sum(x < alphas))
+    # params = params.applymap(lambda x: "%.4f" % x)
+    return params
 
-def logit_summary(results, alpha=0.05, labels=None, pos=1, neg=0, margeff_kwargs={}):
-	summary = results.summary2()
-	summary.tables.pop(1)
-	summary.add_df(logit_params_summary(results, alpha, margeff_kwargs=margeff_kwargs), float_format='%.3f')
-	summary.add_text('Significance level: *: p < 0.1; **: p < 0.05; ***: p < 0.01; ****: p < 0.001')
-	if labels is not None:
-		summary.add_df(logit_evaluation_summary(results, labels, pos, neg), 
-			header=False, index=False, float_format='%.3f')
-	return summary
+def logit_summary(results, labels=None, pos=1, neg=0, float_format='%.4f', summary_kwargs={}):
+    summary = results.summary2()
+    summary.tables.pop(1)
+    summary.add_df(logit_summary_params(results, **summary_kwargs), float_format=float_format)
+    if 'alphas' not in summary_kwargs:
+        summary.add_text('Significance level: *: p < 0.1; **: p < 0.05; ***: p < 0.01; ****: p < 0.001')
+    else:
+        summary.add_text('Significance level: ' + ' '.join(['*'*(i+1) + ': p < {};'.format(a) for i, a in enumerate(summary_kwargs.alphas)]))
+    if labels is not None:
+        summary.add_df(logit_evaluation_summary(results, labels, pos, neg), 
+            header=False, index=False, float_format=float_format)
+    return summary
 
 def plot_coefficients(model, ci=95):
     """
@@ -367,3 +378,42 @@ def plot_logit_marginal_effects(results, ci=95):
     ax.set_yticks(range(n_terms))
     ax.set_yticklabels(coef_names)
     plt.setp(ax.get_xticklabels(), rotation=90)
+
+def summary_to_latex(results):
+    stuff = results.as_latex()
+    caption = re.search(r'(\\caption.+)\n', stuff).group(0)
+    replacements = [
+        # Remove the table environment
+        (r'\\begin{table}\n', '')
+        , (r'\\end{table}\n', '')
+        , (r'\\caption.+\n', '')
+        # Replace it with separate environment for each tabular
+        , (r'(\\begin{center})', r'\\begin{{table}}\n{}\1'.format(caption))
+        , (r'(\\end{center})', r'\1\n\\end{table}')
+        # Remove the bizzare placement of \hline between tables
+        , (r'\\end{table}\n\\hline\n\\begin{table}', r'\\end{table}\n\\begin{table}')
+        # Convert summary results table to longtable
+        , (r'\\begin{tabular}({\w+}\n\\hline\n\s+&\s+Coef.)', r'\\begin{longtable}\1')
+        , (r'(?s)(\\begin{longtable}.*?)(?=\\end{table})', r'\1\\end{longtable}')
+        , (r'(\\end{longtable})\\end{table}', r'\1')
+        , (r'(?s)(\\end{table}\n\\begin{table}.*?)(?=\\begin{longtable})', r'')
+        , (r'(?s)\\end{tabular}(((?!\\end{tabular}).)*)(?=\\end{longtable})', r'')
+        , (r'(\\begin{longtable}{\w+})', r'\1\n{}'.format(caption))
+         # Add a resizebox to fit table to page margins
+    #     , (r'\\begin{tabular}', r'\\resizebox{\\textwidth}{!}{%\n\\begin{tabular}')
+    #     , (r'\\end{tabular}', r'\\end{tabular}}')
+    #     # Merge the table headers and model summary results into a single table
+        , (r'\\end{tabular}\n\\begin{tabular}{\w+}\n(\w+)', r'\1')
+    # #     , (r'lccccccccc', r'lrrrrrrrrr')
+    #     # Using p{8cm} instead of l adds text wrapping for long variable names
+        , (r'(\\begin{longtable}){l(\w+)}', r'\1{p{8cm}\2}')
+    #     # Make double \hlines into single
+        , (r'\\hline\n\\hline', r'\\hline')
+        , (r'\\hline(\nIntercept)', r'\\hline\n\\endhead\n\\hline\n\\multicolumn{10}{r}{\\textit{Continued on next page}} \\\\\\endfoot\n\\hline\n\\endlastfoot\1')
+        , (r'\\end{table}\n\\end{table}', r'\\end{table}')
+    ]
+
+    for old, new in replacements:
+        stuff = re.sub(old, new, stuff)
+
+    return stuff
