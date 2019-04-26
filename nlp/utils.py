9867bbae58152831c90bdc8524005ed35ddaa472
Suraj Iyer <suraj.iyer96@outlook.com>
2019-04-26 15:19:20 +0200

    feat: Added function to correct word compoundings

--- nlp/utils.py
+++ nlp/utils.py
@@ -12,6 +12,8 @@ from os.path import dirname, join
 import numpy as np
 import distance
 from sklearn.cluster import AffinityPropagation
+import nltk
+import pandas as pd
 
 
 def words(text): return re.findall(r'\w+', text.lower())
@@ -43,9 +45,11 @@ def build_trie_from_dict_file(file_path, header=False, columns=[], delimiter=" "
 
         # add all words to a Trie data structure
         if columns:
-            model.addAll(((lambda x: {c: x[i] for i, c in enumerate(columns)})(line.replace('\n', '').split(delimiter)) for line in f.readlines()))
+            model.addAll(((lambda x: {c: x[i] for i, c in enumerate(columns)})(
+                line.replace('\n', '').split(delimiter)) for line in f.readlines()))
         else:
-            model.addAll((line.replace('\n', '').split(delimiter)[0].lower() for line in f.readlines()))
+            model.addAll((line.replace('\n', '').split(delimiter)
+                          [0].lower() for line in f.readlines()))
 
         # call the callback function
         if callback:
@@ -61,7 +65,8 @@ def build_trie_from_dict_file(file_path, header=False, columns=[], delimiter=" "
 def build_trie_from_corpus_file(file_path):
     lang_dict = Counter(words(open(file_path).read()))
     model = Trie()
-    model.addAll(({'word': word, 'count': count} for word, count in lang_dict.items()))
+    model.addAll(({'word': word, 'count': count}
+                  for word, count in lang_dict.items()))
     return model
 
 
@@ -126,12 +131,12 @@ def reduce_lengthening(text):
 def edits_1(word):
     """All edits that are one edit away from `word`."""
     assert isinstance(word, str), '{} must be a string'.format(word)
-    letters         = 'abcdefghijklmnopqrstuvwxyz'
-    splits           = [(word[:i], word[i:])            for i in range(len(word) + 1)]
-    deletes       = [L + R[1:]                             for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:]        for L, R in splits if len(R)>1]
-    replaces     = [L + c + R[1:]                       for L, R in splits if R for c in letters]
-    inserts         = [L + c + R                            for L, R in splits for c in letters]
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    inserts = [L + c + R for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
 
@@ -148,7 +153,8 @@ def _edit_dist(word, dist=2, k=None):
     assert dist > 0 and k > 0
     if k == 1:
         return edits_1(word)
-    new_words = (w2 for w1 in edits_1(word) for w2 in _edit_dist(w1, dist, k - 1))
+    new_words = (w2 for w1 in edits_1(word)
+                 for w2 in _edit_dist(w1, dist, k - 1))
     return list(set(new_words)) if k == dist else new_words
 
 
@@ -159,11 +165,13 @@ def edit_dist(word, dist=2):
 
 
 def cluster_words_by_edit_distance1(words, dist=2):
-    assert isinstance(words, (list, tuple)) and any(isinstance(w, str) for w in words), 'words must be an iterable of strings'
+    assert isinstance(words, (list, tuple)) and any(isinstance(w, str)
+                                                    for w in words), 'words must be an iterable of strings'
     assert isinstance(dist, int)
     cluster = dict()
     for i, w in enumerate(words):
-        x = [words[j] for j in range(i + 1, len(words)) if distance.levenshtein(w, words[j]) == dist]
+        x = [words[j] for j in range(
+            i + 1, len(words)) if distance.levenshtein(w, words[j]) == dist]
         if len(x) > 0:
             cluster[w] = x
     return cluster
@@ -175,15 +183,19 @@ def cluster_words_by_edit_distance2(words, verbose=True, **kwargs):
         L Frey, Brendan J., and Delbert Dueck. "Clustering by passing messages between data points."
         science 315.5814 (2007): 972-976..
     """
-    assert isinstance(words, (list, tuple)) and any(isinstance(w, str) for w in words), 'words must be an iterable of strings'
+    assert isinstance(words, (list, tuple)) and any(isinstance(w, str)
+                                                    for w in words), 'words must be an iterable of strings'
     assert isinstance(verbose, bool)
-    lev_similarity = -1 * np.array([[distance.levenshtein(w1, w2) for w1 in words] for w2 in words])
+    lev_similarity = -1 * \
+        np.array([[distance.levenshtein(w1, w2)
+                   for w1 in words] for w2 in words])
     affprop = AffinityPropagation(affinity="precomputed", **kwargs)
     affprop.fit(lev_similarity)
     clusters = dict()
     for cluster_id in np.unique(affprop.labels_):
         exemplar = words[affprop.cluster_centers_indices_[cluster_id]]
-        clusters[exemplar] = np.unique(words[np.nonzero(affprop.labels_ == cluster_id)])
+        clusters[exemplar] = np.unique(
+            words[np.nonzero(affprop.labels_ == cluster_id)])
         if verbose:
             print(" - *%s:* %s" % (exemplar, ", ".join(clusters[exemplar])))
     return clusters
@@ -249,3 +261,88 @@ def join_bigrams_with_replacements(s, replacements=lambda w, default: default):
             prev_string = string
             string += " " + w[1]
     return string
+
+
+def correct_word_compounding(dfs, language_dictionary, split_on_both_accurate=True, threshold=3):
+    """
+    List all bigrams which are also written as unigrams by removing one space character
+    and filter the ones which contains words not in any dictionary
+
+    Assumptions: every word is unique is only written in one way and there are no spelling errors
+
+    Heuristic:
+        if bigram counts ≈ unibigram counts:
+            # Assumption: both versions are accurate enough
+            if not unibigram in language dictionary:
+                # Assumption: only bigram version accurate
+                split unibigrams to unigrams (make bigram)
+            else:
+                # Assumption: both versions are still accurate enough
+                if both unigrams in language dictionary:
+                    # Assumption: both versions are still accurate enough
+                    # so depend on {:split_on_both_accurate:}
+                    if split_on_both_accurate:
+                        make bigram
+                    else:
+                        convert bigram to unigram by removing space (make unibigram)
+                else:
+                    # Assumption: only unibigram version accurate
+                    make unibigram
+        else:
+            # Assumption: Only one version is accurate
+            if unibigram count >> bigram count:
+                # Assumption: Unibigram version is more accurate
+                if unibigram in language dictionary:
+                    # Assumption: Unibigram version is still more accurate
+                    make unibigram
+                else:
+                    # Assumption: Unibigram version is not accurate so split instead
+                    if both unigrams in language dictionary:
+                        make bigram
+                    else:
+                        make unibigram
+            else:
+                # Assumption: Bigram version is more accurate
+                if both unigrams in language dictionary:
+                        make bigram
+                    else:
+                        make unibigram
+    """
+    assert isinstance(threshold, int) and threshold > 0
+    assert isinstance(split_on_both_accurate, bool)
+    tokens = dfs.str.split(' ')
+    unigrams = set(w for s in tokens for w in s)
+    bigrams = set(" ".join(w) for s in tokens.apply(nltk.bigrams) for w in s)
+    bigrams_also_in_unigrams = set(
+        b for b in bigrams if b.replace(" ", "") in unigrams)
+
+    bigrams = pd.DataFrame([(b, b.replace(" ", ""), b.replace(" ", "-"), dfs.str.contains(b).sum(), dfs.str.contains(b.replace(" ", "") + "|" + b.replace(" ", "-")).sum())
+                            for b in bigrams_also_in_unigrams], columns=['bigram', 'unibigram0', 'unibigram1', 'count_occurence_bigram', 'count_occurence_unibigram'])
+    bigrams['near_equal'] = abs(
+        bigrams["count_occurence_bigram"] - bigrams["count_occurence_unibigram"]) < threshold
+    bigrams['unibigram_in_dict?'] = bigrams['bigram'].apply(
+        lambda bi: bi.replace(" ", "") in language_dictionary)
+    bigrams['unigram0_in_dict?'] = bigrams['bigram'].apply(
+        lambda bi: bi.split(' ')[0] in language_dictionary)
+    bigrams['unigram1_in_dict?'] = bigrams['bigram'].apply(
+        lambda bi: bi.split(' ')[1] in language_dictionary)
+    bigrams['both_unigrams_in_dict?'] = (
+        bigrams['unigram0_in_dict?']) & (bigrams['unigram1_in_dict?'])
+    make_bigram_mask = (((bigrams["near_equal"]) & ((~bigrams["unibigram_in_dict?"])
+        | (bigrams["unibigram_in_dict?"] & bigrams["both_unigrams_in_dict?"] & split_on_both_accurate)))
+    | ((~bigrams["near_equal"]) & (((bigrams["count_occurence_unibigram"] > bigrams["count_occurence_bigram"])
+        & (~bigrams["unibigram_in_dict?"]) & (bigrams['both_unigrams_in_dict?']))
+    | ((bigrams["count_occurence_unibigram"] <= bigrams["count_occurence_bigram"])
+        & (bigrams['both_unigrams_in_dict?'])))))
+
+    # List of word replacements to perform on the text
+    replacements = {}
+    replacements.update({w[1]["unibigram{}".format(i)]: w[1]["bigram"]
+                         for w in bigrams[make_bigram_mask].iterrows() for i in range(2)})
+    replacements.update({w[1]["bigram"]: w[1]["unibigram0"]
+                         for w in bigrams[~make_bigram_mask].iterrows()})
+
+    # Make replacements
+    return tokens.apply(lambda s: [replacements.get(w, w) for w in s])\
+        .apply(nltk.bigrams)\
+        .apply(join_bigrams_with_replacements, replacements=replacements.get)
