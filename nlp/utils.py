0c8a5dd6a5c25f4793b5406442a5b03492b150cd
Suraj Iyer <suraj.iyer96@outlook.com>
2019-05-12 11:19:09 +0200

    feat: Added more regex patterns

--- nlp/utils.py
+++ nlp/utils.py
@@ -365,13 +365,16 @@ def correct_word_compounding(dfs, language_dictionary, split_on_both_accurate=Tr
 class RegexPattern:
     WholeWordOnly = lambda w: r'\b{}\b'.format(w)
     Linebreak = r'\r+|\n+'
-    Number = r'[0-9]+'
+    Number = r"\b[0-9]+([.,][0-9]+)?\b"
     TwoOrMoreSpaces = r'\s{2,}'
     Email = WholeWordOnly(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
     NL_pc4 = WholeWordOnly(r'[0-9]{4}\s?\w{2}')
-    SingleWordCharacter = WholeWordOnly(r'\w')
-    Fractions = r"[0-9]+\.[0-9]+"
-    TimeOfDay = r"[0-2]?[0-9]\:[0-9]{2}\s?(am|pm|AM|PM)?"
+    SingleCharacterWord = WholeWordOnly(r'\w')
+    TimeOfDay = r"([0[0-9]|1[0-9]|2[0-3]):[0-5][0-9](\s?a[.]?m[.]?|p[.]?m[.]?|A[.]?M[.]?|P[.]?M[.]?)?"
     Unicode = r"[^\x00-\x7F]"
     Quotes = lambda x: r"(\"{0}\"|\'{0}\')".format(x)
-    URL = r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)'
+    URL = r'(https?:\/\/(www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)'
+    SpecialCharacters = r'[`!@#*-+{}\[\]:;|\\,<>\/"]'
+    NumbersWithSuffix = r'[0-9]+(st|th|nd|rd)'
+    VersionNumber3N = r"\b(\d+\.)?(\d+\.)?(\*|\d+)\b"
+    Copyright = r"\(c\)|®|©|™"
