0e9aaae313c432b2ddbdf8a9e217e09c9863fd34
Suraj Iyer <suraj.iyer96@outlook.com>
2019-11-12 23:16:40 +0100

    feature: Added general utils module * Added conf/catalog.yml to store predefined collection of datasets. * Added conf/basic.yml to store basic library parameters. * Added general utils module with:     - functions to lazy load datasets from catalog.yml to cache directory     - other general functions * Added verified test cases for general utils module

--- python/python_data_utils/tests/test_utils.py
+++ python/python_data_utils/tests/test_utils.py
@@ -1,31 +1,40 @@
 import pandas as pd
 import os
+import shutil
 from .. import utils as pyu
 
 
 def test_load_dataset_1():
     # test basic loading of existing file from cache directory
-    data = pyu.load_dataset('dutch_dictionary_small',
-                            os.path.join(__file__, '..', '..'))
+    data = pyu.load_artifact('dutch_dictionary_small',
+                             os.path.join(__file__, '..', '..'))
     assert isinstance(data, pd.DataFrame)
     assert data.columns.tolist() == ['word', 'count']
     assert data.shape == (50000, 2)
 
 
-# def test_load_dataset_2():
-#     # test creation of default cache directory
-#     data = pyu.load_dataset('dutch_dictionary_small')
-#     assert os.path.isdir(pyu.DEFAULT_CACHE_DIR)
-#     assert isinstance(data, pd.DataFrame)
-#     assert data.columns.tolist() == ['word', 'count']
-#     assert data.shape == (50000, 2)
-
-
 def test_get_catalog():
+    # test getting the catalog
     assert isinstance(pyu.get_catalog(), dict)
 
 
+def test_load_dataset_2():
+    # test creation of default cache directory
+    # test loading from url
+    name = 'dutch_dictionary_small'
+    data = pyu.load_artifact(name)
+    assert os.path.isdir(pyu._DEFAULT_CACHE_DIR)
+    assert os.path.isfile(pyu.fix_path(os.path.join(
+        pyu._DEFAULT_CACHE_DIR,
+        pyu.get_catalog()[name]['filepath'])))
+    assert isinstance(data, pd.DataFrame)
+    assert data.columns.tolist() == ['word', 'count']
+    assert data.shape == (50000, 2)
+    shutil.rmtree(pyu._DEFAULT_CACHE_DIR, ignore_errors=True)
+
+
 def test_generate_random_string():
+    # test generation of random string with default parameters
     string = pyu.generate_random_string()
     assert len(string) == 10
     assert all(c.isupper() or c.isdigit() for c in string)
---
+++ python/python_data_utils/utils.py
@@ -0,0 +1,166 @@
+# coding: utf-8
+
+"""
+    description: General utility functions / methods
+    author: Suraj Iyer
+"""
+
+__all__ = [
+    'load_from_url',
+    'load_artifact',
+    'clear_cache',
+    'get_catalog',
+    'generate_random_string'
+]
+
+import yaml
+import urllib.request
+import progressbar as pb
+import dill
+import shutil
+import pandas as pd
+from typing import Any
+import os
+
+
+def _load_catalog():
+    with open(os.path.join(__file__, '..', 'conf', "catalog.yml")) as stream:
+        return yaml.safe_load(stream)
+
+
+def get_catalog():
+    import copy
+    return copy.deepcopy(_catalog)
+
+
+def fix_path(path: str) -> str:
+    assert isinstance(path, str)
+    return os.path.abspath(os.path.expanduser(path))
+
+
+# Load default cache dir
+with open(os.path.join(__file__, '..', 'conf', "basic.yml")) as stream:
+    _DEFAULT_CACHE_DIR = fix_path(
+        yaml.safe_load(stream)['default_cache_dir'])
+
+# Load dataset catalog
+_catalog = _load_catalog()
+
+
+def load_from_url(url: str, save_path: str):
+    # Create progressbar
+    widgets = ['Downloaded: ', pb.Percentage(),
+               ' ', pb.Bar(marker=pb.RotatingMarker()),
+               ' ', pb.ETA(),
+               ' ', pb.FileTransferSpeed()]
+    pbar = pb.ProgressBar(widgets=widgets)
+
+    def dl_progress(count, blockSize, totalSize):
+        if pbar.max_value is None:
+            pbar.max_value = totalSize
+            pbar.start()
+        pbar.update(min(count * blockSize, totalSize))
+
+    # Create the save path if not exists
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    # Download the file
+    print(f'Downloading file from {url}.')
+    urllib.request.urlretrieve(url, save_path, reporthook=dl_progress)
+
+    # Close progressbar
+    pbar.finish()
+
+
+def _load_file(path: str, file_type: str, configs: dict = None) -> Any:
+    # open file once cached
+    if file_type == 'txt':
+        with open(path, 'r', **configs) as f:
+            data = f.read()
+    elif file_type == "pkl":
+        data = dill.load(path, **configs)
+    elif file_type == 'csv':
+        configs.pop('filepath_or_buffer', None)
+        data = pd.read_csv(path, **configs)
+    elif file_type == 'xlsx':
+        configs.pop('io', None)
+        data = pd.read_excel(path, **configs)
+    elif file_type == 'parquet':
+        configs.pop('path', None)
+        data = pd.read_parquet(path, **configs)
+    else:
+        raise NotImplementedError('Unknown file type.')
+
+    return data
+
+
+def load_artifact(name: str, cache_dir: str = None) -> Any:
+    """
+    Load any artifact from the catalog.
+
+    :param name: str
+        name of the artifact as defined in the catalog.
+    :param cache_dir: str
+        Directory to cache it in.
+    :return: Any
+    """
+    catalog = get_catalog()
+    assert name in catalog, 'Cannot find given dataset.'
+    assert cache_dir is None or isinstance(cache_dir, str)
+
+    # if cache directory not given
+    if cache_dir is None:
+        # set default cache directory
+        cache_dir = _DEFAULT_CACHE_DIR
+    else:
+        cache_dir = fix_path(cache_dir)
+
+    # if cache directory does not exist
+    if not os.path.isdir(cache_dir):
+        os.mkdir(cache_dir)
+
+    # load data
+    filepath = catalog[name].pop('filepath', None)
+    url = catalog[name].pop('url', None)
+    filetype = catalog[name].pop('type', None)
+    if filepath is None:
+        if url is None:
+            raise ValueError(f"Dataset '{name}' not available.")
+        else:
+            filepath = fix_path(os.path.join(cache_dir, os.path.split(url)[1]))
+            if not os.path.isfile(filepath):
+                load_from_url(url, cache_dir)
+    elif not os.path.isfile(os.path.join(cache_dir, filepath)):
+        if url is None:
+            raise ValueError(f"Dataset '{name}' not available.")
+        else:
+            filepath = fix_path(os.path.join(cache_dir, filepath))
+            load_from_url(url, filepath)
+    else:
+        filepath = fix_path(os.path.join(cache_dir, filepath))
+
+    return _load_file(filepath, filetype, catalog[name])
+
+
+def clear_cache(cache_dir: str = None):
+    if cache_dir is None:
+        cache_dir = _DEFAULT_CACHE_DIR
+    if os.path.isdir(cache_dir):
+        shutil.rmtree(cache_dir, ignore_errors=True)
+
+
+def generate_random_string(characters: str = None, N: int = 10) -> str:
+    """
+    Create randomly generated strings.
+
+    :param characters: str
+        character set to sample from.
+    :param N: int
+        final string length
+    :return: str
+    """
+    import string
+    import random
+    if characters is None:
+        characters = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(N))
