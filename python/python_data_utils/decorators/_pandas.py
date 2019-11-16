7910710dc5f16e8a489c578394fc8205c01feaf9
Suraj Iyer <suraj.iyer96@outlook.com>
2019-11-16 19:32:54 +0100

    feat: Make package zip safe with importlib.resources

--- python/python_data_utils/decorators/_pandas.py
+++ python/python_data_utils/decorators/_pandas.py
@@ -7,26 +7,27 @@
 
 from pandas import DataFrame, Series
 import python_data_utils.pandas.utils as pdu
-import os.path as path
+import importlib.resources as pkg_resources
 
 
-with open(path.join(path.dirname(__file__), "..", "pandas", "utils.py")) as f:
-    f = f.readlines()
+with pkg_resources.path('python_data_utils.pandas', 'utils.py') as p:
+    with open(p) as f:
+        f = f.readlines()
 
-    # Find function names which takes "df" as first input
-    # parameter for DataFrame
-    df_functions = [
-        l.split(" ")[1].split("(")[0] for l in f
-        if l.startswith('def') and ('(df,' in l or '(df)' in l)]
+        # Find function names which takes "df" as first input
+        # parameter for DataFrame
+        df_functions = [
+            l.split(" ")[1].split("(")[0] for l in f
+            if l.startswith('def') and ('(df,' in l or '(df)' in l)]
 
-    # Find function names which takes "s" as first input
-    # parameter for Series
-    series_functions = [
-        l.split(" ")[1].split("(")[0] for l in f
-        if l.startswith('def') and ('(s,' in l or '(s)' in l)]
+        # Find function names which takes "s" as first input
+        # parameter for Series
+        series_functions = [
+            l.split(" ")[1].split("(")[0] for l in f
+            if l.startswith('def') and ('(s,' in l or '(s)' in l)]
 
-for fun in df_functions:
-    setattr(DataFrame, fun, getattr(pdu, fun))
+    for fun in df_functions:
+        setattr(DataFrame, fun, getattr(pdu, fun))
 
-for fun in series_functions:
-    setattr(Series, fun, getattr(pdu, fun))
+    for fun in series_functions:
+        setattr(Series, fun, getattr(pdu, fun))
