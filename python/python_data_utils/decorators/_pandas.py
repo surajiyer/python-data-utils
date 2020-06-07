85ec4327219c7000b944dd71d7b60340ed48d073
Suraj Iyer <suraj.iyer96@outlook.com>
2020-06-07 19:28:32 +0200

    bugfix: skip adding pandas utility functions to pd.DataFrame if Pandas library not installed

--- python/python_data_utils/decorators/_pandas.py
+++ python/python_data_utils/decorators/_pandas.py
@@ -5,29 +5,33 @@
     author: Suraj Iyer
 """
 
-from pandas import DataFrame, Series
 import python_data_utils.pandas.utils as pdu
 import importlib.resources as pkg_resources
 
 
-with pkg_resources.path('python_data_utils.pandas', 'utils.py') as p:
-    with open(p) as f:
-        f = f.readlines()
+try:
+    from pandas import DataFrame, Series
 
-        # Find function names which takes "df" as first input
-        # parameter for DataFrame
-        df_functions = [
-            l.split(" ")[1].split("(")[0] for l in f
-            if l.startswith('def') and ('(df,' in l or '(df)' in l)]
+    with pkg_resources.path('python_data_utils.pandas', 'utils.py') as p:
+        with open(p) as f:
+            f = f.readlines()
 
-        # Find function names which takes "s" as first input
-        # parameter for Series
-        series_functions = [
-            l.split(" ")[1].split("(")[0] for l in f
-            if l.startswith('def') and ('(s,' in l or '(s)' in l)]
+            # Find function names which takes "df" as first input
+            # parameter for DataFrame
+            df_functions = [
+                l.split(" ")[1].split("(")[0] for l in f
+                if l.startswith('def') and ('(df,' in l or '(df)' in l)]
 
-    for fun in df_functions:
-        setattr(DataFrame, fun, getattr(pdu, fun))
+            # Find function names which takes "s" as first input
+            # parameter for Series
+            series_functions = [
+                l.split(" ")[1].split("(")[0] for l in f
+                if l.startswith('def') and ('(s,' in l or '(s)' in l)]
 
-    for fun in series_functions:
-        setattr(Series, fun, getattr(pdu, fun))
+        for fun in df_functions:
+            setattr(DataFrame, fun, getattr(pdu, fun))
+
+        for fun in series_functions:
+            setattr(Series, fun, getattr(pdu, fun))
+except (ModuleNotFoundError, AttributeError):
+    pass
